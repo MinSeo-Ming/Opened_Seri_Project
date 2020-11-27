7bdd6509700371b8172c703edc5c0c25066c8644
MinSeo <chj2171@gmail.com>
2020-11-27 17:09:23 +0900

    내 aws에 서버올리고 도메인도 연결 함.

--- init_db.py
+++ init_db.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.dbsparta
 
 
